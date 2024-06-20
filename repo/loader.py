22af60693508aadb7038d30b1f6134aa804f498e
weilinfox <caiweilin@iscas.ac.cn>
2024-06-20 17:47:17 +0800

    Support MIRROR_SINGLE type
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- repo/loader.py
+++ repo/loader.py
@@ -9,6 +9,7 @@ from utils.github_operation import gh_op
 from utils.logger import logger
 # from utils.error import ParseException
 # from utils.logger import logger
+from .adapters.mirror_adapter import MirrorAdapter
 
 
 class BoardImage:
@@ -26,6 +27,8 @@ class RepoBoardImage:
 
         if image_type == "GITHUB_SINGLE":
             cls = RepoGithubSingleImage
+        elif image_type == "MIRROR_SINGLE":
+            cls = RepoMirrorSingleImage
 
         return object.__new__(cls)
 
@@ -33,12 +36,60 @@ class RepoBoardImage:
         self.title = title
         self.ruyi_repo_cfg = ruyi_repo_cfg
         self.upstream_cfg = upstream_cfg
+        if upstream_cfg is not None:
+            self.image_type = upstream_cfg.get("type", "")
+            self.issue_to = upstream_cfg.get("issueto", "")
 
     def check(self):
         if len(self.ruyi_repo_cfg) > 0:
             logger.info("Skip board image \"{}\"".format(self.title))
         else:
             logger.warn("Skip strange board image")
+
+    def send_issue(self, ood: bool, latest_version: str, missing_files: list[str]):
+        """
+        Send issue
+        :param ood: out of date
+        :param latest_version:
+        :param missing_files:
+        :return:
+        """
+        if self.issue_to == "":
+            logger.warn("In {} issueto not set, issue will not send".format(self.title))
+
+        upstream_title = "IDK"
+        upstream_url = "IDK"
+        latest_url = latest_version
+        if isinstance(self, RepoGithubSingleImage):
+            upstream_title = self.upstream_repo
+            upstream_url = "https://github.com/{}/".format(self.upstream_repo)
+            latest_url = "https://github.com/{}/releases/{}".format(self.upstream_repo, latest_version)
+        elif isinstance(self, RepoMirrorSingleImage):
+            upstream_title = self.mirror_host
+            upstream_url = self.upstream_url
+            latest_url = self.upstream_latest_url
+
+        title = "[ruyi-reimu] Board image {} need update".format(self.title)
+        body = "## Description\n"
+
+        if ood:
+            # send issue
+            logger.warn("In board image {}, the latest version is {}".format(self.title, latest_version))
+            body += ("\n+ In upstream repo [{}]({}), the latest version is [{}]({})"
+                     .format(upstream_title, upstream_url, latest_version, latest_url))
+        else:
+            logger.warn("Board image {} already the latest".format(self.title))
+
+        if len(missing_files):
+            logger.warn("In board image {}, following files not found in upstream releases {}"
+                        .format(self.title, str(missing_files)))
+            body += ("\n+ In upstream repo [{}]({}), these images are missing"
+                     .format(upstream_title, upstream_url))
+            for n in missing_files:
+                body += "\n   +{}".format(n)
+
+        if ood or len(missing_files):
+            gh_op.create_issue(self.issue_to, title, body)
 
 
 class GthubSingleImage(BoardImage):
@@ -53,7 +104,6 @@ class RepoGithubSingleImage(RepoBoardImage):
     def __init__(self, title: str, upstream_cfg: dict, ruyi_repo_cfg: list[dict]):
         super().__init__(title, upstream_cfg, ruyi_repo_cfg)
         self.upstream_repo = upstream_cfg["repo"]
-        self.issueto = upstream_cfg["issueto"]
         self.ruyi_repo = []
         for i in ruyi_repo_cfg:
             self.ruyi_repo.append(GthubSingleImage(list(i.keys())[0], list(i.values())[0]["distfiles"][0]))
@@ -76,17 +126,19 @@ class RepoGithubSingleImage(RepoBoardImage):
             # versions are sort in time,
             # but we cannot sort these version code
             # they could in invalid version format
+            # todo: 版本排序
             if latest == "":
                 latest = u.title
 
             # check assets
+            # todo: 更好的版本匹配
             for a in u.get_assets():
                 get = []
                 for i in range(len(names)):
                     if names[i] == a.name and sizes[i] == a.size:
                         now.append(u.title)
                         get.append(i)
-                    # remove found item
+                # remove found item
                 for i in range(len(get)):
                     names.pop(get[i] - i)
                     sizes.pop(get[i] - i)
@@ -96,34 +148,54 @@ class RepoGithubSingleImage(RepoBoardImage):
             if len(names) == 0:
                 break
 
-        flag = True
-        title = "[ruyi-reimu] Board image {} need update".format(self.title)
-        body = "## Description\n"
+        # out of date
+        ood = True
         for v in now:
             if v == latest:
-                flag = False
+                ood = False
                 break
-        if flag:
-            # send issue
-            logger.warn("In board image {}, the latest version is {}".format(self.title, latest))
-            body += ("\n+ In upstream repo [{0}](https://github.com/{0}/), "
-                     "the latest version is [{1}](https://github.com/{0}/releases/{1})"
-                     .format(self.upstream_repo, latest))
-        else:
-            logger.warn("Board image {} already the latest".format(self.title))
-
-        if len(names):
-            flag = True
-            logger.warn("In board image {}, following files not found in upstream releases {}"
-                        .format(self.title, str(names)))
-            body += ("\n+ In upstream repo [{0}](https://github.com/{0}/), these images are missing"
-                     .format(self.upstream_repo))
-            for n in names:
-                body += "\n   +{}".format(n)
 
         # send issue
-        if flag:
-            gh_op.create_issue(self.issueto, title, body)
+        self.send_issue(ood, latest, names)
+
+
+class MirrorSingleImage(BoardImage):
+    def __init__(self, name: str, distfiles: dict):
+        super().__init__(name, distfiles)
+        self.filename = distfiles["name"]
+
+
+class RepoMirrorSingleImage(RepoBoardImage):
+
+    def __init__(self, title: str, upstream_cfg: dict, ruyi_repo_cfg: list[dict]):
+        super().__init__(title, upstream_cfg, ruyi_repo_cfg)
+        self.mirror_host = upstream_cfg["host"]
+        self.mirror_path = upstream_cfg["path"]
+        self.version_match = upstream_cfg["match"]
+        self.ruyi_repo = []
+        for i in ruyi_repo_cfg:
+            self.ruyi_repo.append(MirrorSingleImage(list(i.keys())[0], list(i.values())[0]["distfiles"][0]))
+
+        self.adapter = MirrorAdapter(self.mirror_host, self.mirror_path)
+        self.upstream_url = self.adapter.get_url()
+        self.upstream_latest_url = self.adapter.get_url() + "latest"
+
+    def check(self):
+        logger.info("Check board image {}".format(self.title))
+
+        # todo: release 排序
+        releases = self.adapter.get_releases(self.version_match)
+        releases.sort(reverse=True)
+        filenames = []
+        ruyi = []
+        for rr in self.ruyi_repo:
+            filenames.append(rr.filename)
+        for r in releases:
+            if self.adapter.find_assets(r, filenames):
+                ruyi.append(releases)
+
+        self.upstream_latest_url = self.upstream_url + "/" + releases[0]
+        self.send_issue(ruyi[0] != releases[0], releases[0], filenames)
 
 
 class Repo:
