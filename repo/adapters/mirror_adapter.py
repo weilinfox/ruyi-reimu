1ab9e1fb199f60c038f9bd424accf6b83ed5fe3e
weilinfox <caiweilin@iscas.ac.cn>
2024-07-03 13:03:11 +0800

    Support download.freebsd.org
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- repo/adapters/mirror_adapter.py
+++ repo/adapters/mirror_adapter.py
@@ -18,6 +18,8 @@ class MirrorAdapter:
             cls = OpenWrtDownloadsMirrorAdapter
         elif host == "cdimage.ubuntu.com":
             cls = UbuntuCdimageMirrorAdapter
+        elif host == "download.freebsd.org":
+            cls = FreebsdDownloadMirrorAdapter
 
         return object.__new__(cls)
 
@@ -164,6 +166,36 @@ class UbuntuCdimageMirrorAdapter(MirrorAdapter):
             if vs is None:
                 continue
             vs = vs.text.strip()
+            if not vs:
+                continue
+            if vs[-1] == '/':
+                vs = vs[:-1]
+            if not version_match or re.match(version_match, vs):
+                vss.append(vs)
+
+        return vss
+
+
+class FreebsdDownloadMirrorAdapter(MirrorAdapter):
+
+    def __init__(self, protocol: str, host: str, path: str):
+        super().__init__(protocol, host, path)
+
+    def get_releases(self, version_match: str) -> list[str]:
+        url = self.get_url()
+        resp = requests.get(url)
+
+        if resp.status_code != 200:
+            raise NetworkException("Get {} get code {}".format(url, str(resp.status_code)))
+
+        soup = BeautifulSoup(resp.text, 'html.parser')
+
+        vss = []
+        for tr in soup.find_all("tr"):
+            vs = tr.a
+            if vs is None:
+                continue
+            vs = vs.text
             if not vs:
                 continue
             if vs[-1] == '/':
