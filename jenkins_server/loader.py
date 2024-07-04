db0a8419e587cccbe1555fc42de923eb9a698e97
weilinfox <caiweilin@iscas.ac.cn>
2024-07-04 20:36:17 +0800

    Job creation
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- jenkins_server/loader.py
+++ jenkins_server/loader.py
@@ -7,17 +7,28 @@ from utils.logger import logger
 
 
 class JenkinsServer:
+    FOLDER_NAME = "ruyi-reimu-mugen-auto-test"
+
     def __init__(self):
         self.server = jenkins.Jenkins(" ")
         self.user = "IDK"
         self.version = "IDK"
+
+        self._host = ""
+        self._username = ""
+        self._password = ""
 
     def load(self):
         if not reimu_config.ready():
             raise Exception("ruyi-reimu not configured")
 
         jenkins_cfg = reimu_config.youmu_jenkins["jenkins"]
-        self.server = jenkins.Jenkins(jenkins_cfg["host"], jenkins_cfg["username"], jenkins_cfg["password"])
+        self._host = jenkins_cfg["host"]
+        self._username = jenkins_cfg["username"]
+        self._password = jenkins_cfg["password"]
+
+        self._new_server()
+
         self.user = self.server.get_whoami()["fullName"]
         logger.info("Login jenkins server with username {}".format(self.user))
         self.version = self.server.get_version()
@@ -27,11 +38,14 @@ class JenkinsServer:
 
         logger.info("Jenkins server check done.\n\n")
 
+    def _new_server(self):
+        self.server = jenkins.Jenkins(self._host, self._username, self._password)
+
     def _jenkins_folder_create(self):
         """
         All jobs in one folder
         """
-        job_name = "ruyi-reimu-mugen-auto-test"
+        job_name = JenkinsServer.FOLDER_NAME
 
         if self.server.job_exists(job_name) and not self.server.is_folder(job_name):
             logger.warn("Jenkins job {} exists but not a folder, delete it".format(job_name))
@@ -59,6 +73,76 @@ class JenkinsServer:
             self.server.reconfig_job(job_name, xml)
         else:
             logger.info("Use existing jenkins job folder {}".format(job_name))
+
+    def _jenkins_job_create(self, job_name: str):
+        """
+        Create job in folder
+        """
+        job_name = "{}/{}".format(JenkinsServer.FOLDER_NAME, job_name)
+
+        if self.server.job_exists(job_name) and self.server.is_folder(job_name):
+            logger.warn("Jenkins job {} exists but is a folder, delete it".format(job_name))
+            self.server.delete_job(job_name)
+
+        if not self.server.job_exists(job_name):
+            logger.info("Create jenkins job {}".format(job_name))
+            self.server.create_job(job_name, JenkinsServer._jenkins_job_gen_xml())
+        else:
+            logger.info("Use existing jenkins job {}".format(job_name))
+
+    @staticmethod
+    def _jenkins_job_gen_xml(labels="", cmd="", artifacts="") -> str:
+        et = ElementTree.fromstring(jenkins.EMPTY_CONFIG_XML)
+
+        shell_ele = ElementTree.Element("hudson.tasks.Shell")
+        cmd_ele = ElementTree.Element("command")
+        cmd_ele.text = cmd
+        shell_ele.append(cmd_ele)
+        shell_ele.append(ElementTree.Element("configuredLocalRules"))
+
+        builders_ele = ElementTree.Element("builders")
+        builders_ele.append(shell_ele)
+
+        archiver_ele = ElementTree.Element("hudson.tasks.ArtifactArchiver")
+        artifacts_ele = ElementTree.Element("artifacts")
+        artifacts_ele.text = artifacts
+        archiver_ele.append(artifacts_ele)
+        empty_archive_ele = ElementTree.Element("allowEmptyArchive")
+        empty_archive_ele.text = "false"
+        archiver_ele.append(empty_archive_ele)
+        on_success_ele = ElementTree.Element("onlyIfSuccessful")
+        on_success_ele.text = "true"
+        archiver_ele.append(on_success_ele)
+        fingerprint_ele = ElementTree.Element("fingerprint")
+        fingerprint_ele.text = "false"
+        archiver_ele.append(fingerprint_ele)
+        ant_exclude_ele = ElementTree.Element("defaultExcludes")
+        ant_exclude_ele.text = "true"
+        archiver_ele.append(ant_exclude_ele)
+        case_sensitive_ele = ElementTree.Element("caseSensitive")
+        case_sensitive_ele.text = "true"
+        archiver_ele.append(case_sensitive_ele)
+        follow_symlinks_ele = ElementTree.Element("followSymlinks")
+        follow_symlinks_ele.text = "true"
+        archiver_ele.append(follow_symlinks_ele)
+
+        publishers_ele = ElementTree.Element("publishers")
+        publishers_ele.append(archiver_ele)
+
+        node_ele = ElementTree.Element("assignedNode")
+        node_ele.text = labels
+
+        for e in et.findall("publishers"):
+            et.remove(e)
+        for e in et.findall("builders"):
+            et.remove(e)
+        for e in et.findall("assignedNode"):
+            et.remove(e)
+        et.append(builders_ele)
+        et.append(publishers_ele)
+        et.append(node_ele)
+
+        return ElementTree.tostring(et).decode('utf-8')
 
 
 youmu_jenkins = JenkinsServer()
