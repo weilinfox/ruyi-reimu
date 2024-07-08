bb47f1d22c542eb43a057c0a9863be335948548a
weilinfox <caiweilin@iscas.ac.cn>
2024-07-08 22:04:11 +0800

    Add testing control
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- jenkins_controller/script.py
+++ jenkins_controller/script.py
@@ -1,18 +1,24 @@
 from utils.errors import AssertException
 
-class ScriptGenerator():
+
+class ScriptGenerator:
     CMD_UPGRADE = {"debian": "{0} apt-get update && {0} apt-get upgrade -y",
                    "fedora": "{0} dnf upgrade -y",
                    "archlinux": "{0} pacman --noconfirm -Syuu",
                    "gentoo": "{0} emerge-webrsync"}
-    CMD_MUGEN_DEP_INSTALL = {"debian": "{0} apt-get install -y sudo file expect git psmisc iputils-ping make python3-paramiko python3-six tar jq",
-                             "fedora": "{0} dnf install -y sudo file expect git psmisc make iputils python3-six python3-paramiko tar jq",
-                             "archlinux": "{0} pacman --need --noconfirm -S sudo file expect git psmisc make iputils python-six python-paramiko tar jq",
-                             "gentoo": "{0} emerge --color=n --getbinpkg --noreplace --autounmask=y app-admin/sudo sys-apps/file dev-tcltk/expect dev-vcs/git sys-process/psmisc dev-build/make dev-python/paramiko dev-python/six app-arch/tar app-misc/jq"}
+    CMD_MUGEN_DEP_INSTALL = {"debian": "{0} apt-get install -y sudo file expect git psmisc iputils-ping make"
+                                       " python3-paramiko python3-six tar jq",
+                             "fedora": "{0} dnf install -y sudo file expect git psmisc make iputils python3-six"
+                                       " python3-paramiko tar jq",
+                             "archlinux": "{0} pacman --need --noconfirm -S sudo file expect git psmisc make iputils"
+                                          " python-six python-paramiko tar jq",
+                             "gentoo": "{0} emerge --color=n --getbinpkg --noreplace --autounmask=y app-admin/sudo"
+                                       " sys-apps/file dev-tcltk/expect dev-vcs/git sys-process/psmisc dev-build/make"
+                                       " dev-python/paramiko dev-python/six app-arch/tar app-misc/jq"}
 
-
-    def __init__(self, gen_type: str, disto_type: str, cfg: dict):
-        self.disto_type = disto_type
+    def __init__(self, gen_type: str, distro_type: str, cfg: dict):
+        self.distro_type = distro_type
+        self.gen_type = gen_type
         self.cfg = cfg
         self.cmds = []
         # 默认为普通用户
@@ -25,16 +31,16 @@ class ScriptGenerator():
         elif gen_type == "upgrade":
             self._gen_upgrade_cmds()
         else:
-            raise AssertException("Unknown command generate type {}".format(gen_type)
+            raise AssertException("Unknown command generate type {}".format(gen_type))
 
     def _gen_mugen_test_cmds(self):
         self._gen_upgrade_cmds()
 
-        self.cmds.append(CmdGenerator.CMD_MUGEN_DEP_INSTALL[self.disto_type])
+        self.cmds.append(ScriptGenerator.CMD_MUGEN_DEP_INSTALL[self.distro_type])
 
         self.cmds.extend(["git clone --depth=1 https://gitee.com/ruyisdk/ruyi-mugen.git .",
                           "mkdir -p conf",
-                          """echo '{ "NODE": [{ "ID": 1,
+                          """echo '{{ "NODE": [{{ "ID": 1,
     "LOCALTION": "local",
     "MACHINE": "physical",
     "IPV6": "::1",
@@ -47,7 +53,7 @@ class ScriptGenerator():
     "SSH_PORT": 22,
     "BMC_IP": "",
     "BMC_USER": "",
-    "BMC_PASSWORD": "" }]}' > conf/env.json""",
+    "BMC_PASSWORD": "" }}]}}' > conf/env.json""",
                           "bash ruyi_mugen.sh -f ruyi -x 2>&1 | tee report_gen_tmpl/26test_log.md",
                           "bash report_gen.sh {1}",
                           "{0} chown -R $USER:$USER ./* ./.*",
@@ -57,13 +63,13 @@ class ScriptGenerator():
                           "mv ruyi_report/*.md ."])
 
     def _gen_upgrade_cmds(self):
-        self.cmds.append(CmdGenerator.CMD_UPGRADE[self.disto_type])
+        self.cmds.append(ScriptGenerator.CMD_UPGRADE[self.distro_type])
 
     def get_script(self) -> str:
         cmds = ""
         for c in self.cmds:
             cmds += c + "\n"
-        return cmds.format("sudo" if self.sudo else "", self.disto_type)
+        return cmds.format("sudo" if self.sudo else "", self.distro_type)
 
     def get_artifacts(self) -> str:
         if self.gen_type != "mugen":
